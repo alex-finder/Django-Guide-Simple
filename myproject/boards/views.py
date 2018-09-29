6cb43fea7c9b9c09c9648b6c34527f3be48de456
Alex Finder <alexandru.finder@yahoo.com>
2018-09-29 12:28:12 +0300

    Added lines from tutorial 3

--- myproject/boards/views.py
+++ myproject/boards/views.py
@@ -15,3 +15,7 @@ def home(request):
     # response_html = '<br>'.join(boards_names)
     # return HttpResponse(response_html)
     return render(request, 'home.html', {'boards': boards})
+
+def board_topics(request, pk):
+    board = Board.objects.get(pk=pk)
+    return render(request, 'topics.html', {'board': board})
